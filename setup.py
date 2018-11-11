35a1be3094a77ef48c10cea01ccee1a36d183771
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2018-11-11 23:32:16 +0300

    * различные мелкие настройки

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_packages
 import gov
 
 setup(
-    name="gov-zakupki-crawler",
+    name="gov-purchases-crawler",
     version=gov.__version__,
     packages=find_packages(exclude=['tests']),
     author="Alexander Ruzhnikov",
     author_email="ruzhnikov85@gmail.com",
     license="MIT",
     python_requires='>=3.7.0',
-    install_requires=["asyncio", "lxml", "clickhouse-driver"],
+    install_requires=["asyncio", "lxml", "psycopg2-binary"],
     description="Crawler of resources from ftp.zakupki.gov.ru",
     entry_points={
         "console_scripts": [
